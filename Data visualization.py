818dc9dded61f888e617ecccd4e9d9916e35dd08
cherencia <herencia.carlos@gmail.com>
2020-11-22 00:09:34 +0100

    Final dataframe

--- Data visualization.py
+++ Data visualization.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
-import datetime as date
+
 
 # Load the data and create dataframes
 os.chdir(r'C:\Users\Carlosh\github\Master-Thesis')
@@ -21,6 +21,7 @@ Dataframe_02.head()
 Dataframe_02["logtime"] = Dataframe_02["logtime"].str.split("T",
                                                             n=1, expand=True)
 Dataframe_02["logtime"] = pd.to_datetime(Dataframe_02["logtime"])
+Dataframe_02 = Dataframe_02.dropna(subset=['attr_price_num', 'logtime'])
 
 Dataframe_02.groupby('user_id')['attr_price_num'].mean()
 
@@ -31,13 +32,33 @@ def GetMonths(t0, date_today):
     delta = date_today - t0
     months = int(delta.days/30)
     return months
+
+
+# Create function that eludes dividing by zero
+def Division(n, d):
+    return n / d if d else 0
+
+
 # Function that calculates average return per user
-
-
 def Return_perUser(dataframe, date_today):
+    date_today = pd.to_datetime(date_today)
     unique_users = pd.Series(dataframe['user_id'].unique())
+    average_revenue = []
     for user in unique_users:
         subsection = dataframe.loc[dataframe['user_id'] == user]
+        revenue_of_user = subsection['attr_price_num']
         oldest_date = min(subsection['logtime'])
         months = GetMonths(oldest_date, date_today)
-        avg_monthly_revenue =
+        total_revenue = revenue_of_user.sum(axis=0)
+        avg_monthly_revenue = Division(total_revenue, months)
+        average_revenue.append(avg_monthly_revenue)
+    dataframe_converted = unique_users.to_frame(name='user_id')
+    dataframe_converted['avg_revenue'] = average_revenue
+    return dataframe_converted
+
+
+# Now we have a dataframe with the monthly return on average
+avg_monthly_revenue = Return_perUser(Dataframe_02, '2020-11-21')
+
+
+# Merge datasets
