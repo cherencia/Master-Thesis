a4984bd7fcb81ffdc723c443257cda8c28df804f
cherencia <herencia.carlos@gmail.com>
2020-12-02 18:04:40 +0100

    This dataframe will not be used

--- Data visualization.py
+++ Data visualization.py
@@ -8,15 +8,14 @@ import datetime
 # Load the data and create dataframes
 
 
-Dataframe = pd.read_csv(
-          r'Final_dataframe.csv')
+Dataframe = pd.read_csv(r'mega_dataframe.csv')
 Dataframe.head()
 Dataframe["logtime"] = pd.to_datetime(Dataframe["logtime"])
 Dataframe.groupby('nc')['attr_price_num'].mean()
 # Drop e_commerce purchase values (they are repeated)
 
 
-df = Dataframe[Dataframe['event_action']=='Payment Completed']
+df = Dataframe[Dataframe['event_action'] == 'Payment Completed']
 df = df.loc[Dataframe['attr_label_str'] != 'loyalty']
 
 
@@ -57,23 +56,22 @@ def Return_perUser(dataframe, date_today):
 
 # Now we have a dataframe with the monthly return on average
 avg_monthly_revenue = Return_perUser(df, '2020-11-21 20:29:14.169000+00:00')
-avg_monthly_revenue_merged = avg_monthly_revenue.merge(df[['user_id',
-                                                                  'nc',
-                                                                  'devicebrand',
-                                                                  'attr_os_str']],
-                                                on='user_id',how= 'inner')
+avg_monthly_revenue_merged = avg_monthly_revenue.merge(df[['user_id','nc',
+                                                           'devicebrand',
+                                                           'attr_os_str',
+                                                           'cnt_call',
+                                                           'cnt_dis']],
+                                                           on='user_id',
+                                                           how= 'inner')
 avg_monthly_revenue_unique = avg_monthly_revenue_merged.drop_duplicates(keep=
                                                                         'first',
                                                                         subset=
                                                                         'user_id')
 avg_monthly_revenue_unique.isna().mean()
-
+avg_monthly_revenue_unique.loc[avg_monthly_revenue_unique['cnt_call']==0]
 # Merge both dataframes
 Dataframe_final = df.merge(avg_monthly_revenue, on='user_id')
-
-
-
-#Plots
+# Plots
 ios_or_android = avg_monthly_revenue_unique.groupby(
     'attr_os_str')['attr_os_str'].count()
 ios_or_android.plot.bar()
@@ -81,4 +79,3 @@ natco = avg_monthly_revenue_unique.groupby('nc')['nc'].count()
 natco.plot.bar()
 ARPU = avg_monthly_revenue_unique.groupby('nc')['avg_revenue'].mean()
 ARPU.plot.bar()
-print(os.getcwd())
